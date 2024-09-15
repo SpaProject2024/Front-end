bafb6e020272822868f840049e9d9cf527711533
NguyenThiNga111 <nguyentronghuu.07052003@gmail.com>
2024-09-16 00:38:41 +0700

    nga/features/login

---
+++ app/Fogetpassword/fogetpassword.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class fogetpassword extends Component {
+  render() {
+    return (
+      <View>
+        <Text>fogetpassword</Text>
+      </View>
+    )
+  }
+}
---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+// app/_layout.tsx
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="login"/>
+      <Stack.Screen name="register"/>
+      <Stack.Screen name="fogetpassword"/>
+    </Stack>
+  );
+}
